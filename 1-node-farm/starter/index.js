4d9700133b41a7f7438fa4655acd0931cbc13108
AgnesYichenFeng <50085326+AgnesYichenFeng@users.noreply.github.com>
2023-01-11 15:07:53 -0500

    routing and simple api

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,71 @@
+//Server
+
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%ProductName%}/g, product.productName);
+  output = output.replace(/{%Image%}/g, product.image);
+  output = output.replace(/{%From%}/g, product.from);
+  output = output.replace(/{%Price%}/g, product.price);
+  output = output.replace(/{%Nutrients%}/g, product.nutrients);
+  output = output.replace(/{%Quantity%}/g, product.quantity);
+  output = output.replace(/{%Description%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%Not_Organic%}/g, 'not-organic');
+  }
+
+  return output;
+}
+
+
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+//Templates
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  //Overview page
+  if (pathName === '/' || pathName === '/overview') {
+    const cardsHtml = dataObject.map(element => replaceTemplate(tempCard, element)).join('');
+    const output = tempOverview.replace('{%Product_Cards%}', cardsHtml);
+
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.end(output);
+
+    //Product page
+  } else if (pathName == '/product') {
+    res.end('This is product')
+
+    // api
+  } else if (pathName == '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json'
+    });
+    res.end(data);
+
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello - yichen'
+    });
+    res.end('<h1>Page not found<h1>')
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000')
+});
+
+
+
+
+
